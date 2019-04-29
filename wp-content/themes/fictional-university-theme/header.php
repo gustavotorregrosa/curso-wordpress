afcdd62c04dad6b455aeafe24a5ca38fa9d3089f
Gustavo Torregrosa <gustavo@visie.com.br>
2019-04-29 11:14:24 -0300

    partial

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes() ?>>
 <head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head() ?>
 </head>
-<body>
+<body <?php body_class() ?>>
     <header class="site-header">
         <div class="container">
-        <h1 class="school-logo-text float-left"><a href="#"><strong>Fictional</strong> University</a></h1>
+        <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
+                <?php 
+                // wp_nav_menu(
+                //     [
+                //         'theme_location' => 'headerMenuLocation'
+                //     ]
+                // ) 
+                ?>
             <ul>
-                <li><a href="#">About Us</a></li>
+                <li <?php if(is_page('sobre-mim') or wp_get_post_categories(0) == '13') echo 'class="current-menu-item"' ?> ><a href="<?php echo site_url('/sobre-mim')?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
